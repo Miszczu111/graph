36365c467a8894c9a5e3975bc3165ce0899649a2
Miszczu111 <dawid00257@tlen.pl>
2015-01-09 12:06:20 +0100

    Init

---
+++ Program-grafy/Program-grafy/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Program_grafy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.czytajToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rysujToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grafToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grafDopełniajacyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.razemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grafNaKoleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grafToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.razemNaKoleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.raportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtList = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.czytajToolStripMenuItem,
+            this.rysujToolStripMenuItem,
+            this.raportToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(863, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // czytajToolStripMenuItem
+            // 
+            this.czytajToolStripMenuItem.Name = "czytajToolStripMenuItem";
+            this.czytajToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.czytajToolStripMenuItem.Text = "Czytaj";
+            this.czytajToolStripMenuItem.Click += new System.EventHandler(this.czytajToolStripMenuItem_Click);
+            // 
+            // rysujToolStripMenuItem
+            // 
+            this.rysujToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.grafToolStripMenuItem,
+            this.grafDopełniajacyToolStripMenuItem,
+            this.razemToolStripMenuItem,
+            this.grafNaKoleToolStripMenuItem,
+            this.grafToolStripMenuItem1,
+            this.razemNaKoleToolStripMenuItem});
+            this.rysujToolStripMenuItem.Name = "rysujToolStripMenuItem";
+            this.rysujToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.rysujToolStripMenuItem.Text = "Rysuj";
+            // 
+            // grafToolStripMenuItem
+            // 
+            this.grafToolStripMenuItem.Name = "grafToolStripMenuItem";
+            this.grafToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.grafToolStripMenuItem.Text = "Graf";
+            this.grafToolStripMenuItem.Click += new System.EventHandler(this.grafToolStripMenuItem_Click);
+            // 
+            // grafDopełniajacyToolStripMenuItem
+            // 
+            this.grafDopełniajacyToolStripMenuItem.Name = "grafDopełniajacyToolStripMenuItem";
+            this.grafDopełniajacyToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.grafDopełniajacyToolStripMenuItem.Text = "Graf dopełniający";
+            this.grafDopełniajacyToolStripMenuItem.Click += new System.EventHandler(this.grafDopełniajacyToolStripMenuItem_Click);
+            // 
+            // razemToolStripMenuItem
+            // 
+            this.razemToolStripMenuItem.Name = "razemToolStripMenuItem";
+            this.razemToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.razemToolStripMenuItem.Text = "Razem";
+            this.razemToolStripMenuItem.Click += new System.EventHandler(this.razemToolStripMenuItem_Click);
+            // 
+            // grafNaKoleToolStripMenuItem
+            // 
+            this.grafNaKoleToolStripMenuItem.Name = "grafNaKoleToolStripMenuItem";
+            this.grafNaKoleToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.grafNaKoleToolStripMenuItem.Text = "Graf na kole";
+            // 
+            // grafToolStripMenuItem1
+            // 
+            this.grafToolStripMenuItem1.Name = "grafToolStripMenuItem1";
+            this.grafToolStripMenuItem1.Size = new System.Drawing.Size(207, 22);
+            this.grafToolStripMenuItem1.Text = "Graf dopełniający na kole";
+            // 
+            // razemNaKoleToolStripMenuItem
+            // 
+            this.razemNaKoleToolStripMenuItem.Name = "razemNaKoleToolStripMenuItem";
+            this.razemNaKoleToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.razemNaKoleToolStripMenuItem.Text = "Razem na kole";
+            // 
+            // raportToolStripMenuItem
+            // 
+            this.raportToolStripMenuItem.Name = "raportToolStripMenuItem";
+            this.raportToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.raportToolStripMenuItem.Text = "Raport";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtList);
+            this.groupBox3.Location = new System.Drawing.Point(19, 28);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(132, 130);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Odczytana lista";
+            // 
+            // txtList
+            // 
+            this.txtList.Location = new System.Drawing.Point(7, 20);
+            this.txtList.Multiline = true;
+            this.txtList.Name = "txtList";
+            this.txtList.Size = new System.Drawing.Size(120, 104);
+            this.txtList.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(366, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Graf";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(657, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Graf dopełniający";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 513);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem czytajToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rysujToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grafToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grafDopełniajacyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem raportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem razemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grafNaKoleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grafToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem razemNaKoleToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Program-grafy/Program-grafy/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_grafy
+{
+    public partial class Form1 : Form
+    {
+        int[,] tab;
+        int n, f;
+        Dictionary<int, int[]> dict = new Dictionary<int, int[]>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void czytajToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            of.ShowDialog();
+            
+            StreamReader sr = new StreamReader(of.FileName);
+                     
+            string line;
+            int k = 0;
+            tab = new int[6, 6];
+            //tab = new int[sr.ReadLine().Length/2 + 1, sr.ReadLine().Length/2 + 1];
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] s = line.Split(new string[] { " " },
+                    StringSplitOptions.RemoveEmptyEntries);                
+                for (int i = 0; i < s.Length; i++)
+                {                    
+                    tab[k,i] = Convert.ToInt32(s[i]);
+                }
+                k++;
+            }
+
+            //Wyświetlenie
+            for (int i = 0; i < tab.GetLength(1); i++)
+            {
+                for (int j = 0; j < tab.GetLength(0); j++)
+                {
+                    txtList.Text += tab[i, j] + " ";
+                }
+                txtList.Text += "\r\n";
+            }
+        }
+
+        private void grafToolStripMenuItem_Click(object sender, EventArgs e)
+        {            
+            rysowanie(380,150, 0);
+        }
+
+        private void rysowanie(int xi, int yi, int coRysujemy)
+        {
+            n = tab.GetLength(0);
+
+            dict.Clear();
+
+            double alfa = 360 / n; //kat pomiedzy rozmieszczonymi na okregu wierzchołkami
+
+            Image wierzcholek = Image.FromFile(@".\punkt.png"); //obrazek wierzchołka
+
+            System.Drawing.Graphics graphicsObj;
+            graphicsObj = this.CreateGraphics();
+            Pen myPen = new Pen(System.Drawing.Color.DarkRed, 3); //do pisania stopni wierzchołków            
+            int key = 0;
+
+            Font myFont = new Font("Arial", 11); // czcionka do opisywania wierzchołków
+
+            for (int i = 0; i < n; i++) //pętla przechodząca po wszystkich wierzchołkach
+            {
+                double kat = alfa * (i + 1); //aktualny kąt (potrzebne do rysowania)
+
+                double x = xi + 100 * Math.Cos(2 * Math.PI * i / n); //oblicznie współrzędnych wierzchołka
+                double y = yi + 100 * Math.Sin(2 * Math.PI * i / n);
+
+                //double x = 560 + 200 * Math.Cos(2 * Math.PI * i / n); //oblicznie współrzędnych wierzchołka
+                //double y = 250 + 200 * Math.Sin(2 * Math.PI * i / n);
+
+                int xk = Convert.ToInt32(x); //konwersja (potrzebna do funkcji rysowania)
+                int yk = Convert.ToInt32(y);
+
+                int[] wsp = new int[2]; //tablica przechowująca pare współrzędnych danego wierzchołka
+                wsp[0] = xk;
+                wsp[1] = yk;
+
+                dict.Add(key, wsp); //dodanie wierzchołka z jego współrzędnymi do dictionary
+                key++; //inkrementacja klucza dictionary
+                int stopien = 0; //zmienna do przechowania stopnia aktualnego wierzchołka w pętli
+
+                graphicsObj.DrawImage(wierzcholek, xk, yk); // rysowanie wierzchołków
+
+                //obliczanie stopnia aktualnego wierzchołka
+                for (int j = 0; j < n; j++)
+                {
+                    stopien += tab[i, j];
+                }
+
+                //Opisywanie stopnia każdego wierzchołka
+                //if ((yk <250) && (xk < 560))
+                //    graphicsObj.DrawString(stopien.ToString(), myFont, Brushes.DarkRed, new PointF(xk - 19, yk - 14));
+                //else if ((yk >= 250) && (xk < 560))
+                //    graphicsObj.DrawString(stopien.ToString(), myFont, Brushes.DarkRed, new PointF(xk - 19, yk + 14));
+                //else if ((yk >= 250) && (xk >= 560))
+                //    graphicsObj.DrawString(stopien.ToString(), myFont, Brushes.DarkRed, new PointF(xk + 10, yk + 14));
+                //else if ((yk < 250) && (xk >= 560))
+                //    graphicsObj.DrawString(stopien.ToString(), myFont, Brushes.DarkRed, new PointF(xk + 10, yk - 14));
+
+                //numerowanie wierzchołków
+                if ((yk < 250) && (xk < 560))
+                    graphicsObj.DrawString((i + 1).ToString(), myFont, Brushes.DarkRed, new PointF(xk - 19, yk - 14));
+                else if ((yk >= 250) && (xk < 560))
+                    graphicsObj.DrawString((i + 1).ToString(), myFont, Brushes.DarkRed, new PointF(xk - 19, yk + 14));
+                else if ((yk >= 250) && (xk >= 560))
+                    graphicsObj.DrawString((i + 1).ToString(), myFont, Brushes.DarkRed, new PointF(xk + 10, yk + 14));
+                else if ((yk < 250) && (xk >= 560))
+                    graphicsObj.DrawString((i + 1).ToString(), myFont, Brushes.DarkRed, new PointF(xk + 10, yk - 14));
+            }
+
+            int Pwspx = 0, Pwspy = 0; //wsp pierwszego wierzchołka 
+            int Dwspx = 0, Dwspy = 0; //wsp drugiego wierzchołka  - Pomiedzy nimi rysujemy krawędź
+            Pen zielony = new Pen(System.Drawing.Color.Green, 1);
+            Pen czarny = new Pen(System.Drawing.Color.Black, 1);
+            //petla chodzaca po macierzy krawędzi
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    switch (coRysujemy)
+                    {
+                        case 0:
+                            if (tab[i, j] == 1 && i != j) //jesli natrafiamy na 1 w macierzy to:                    
+                                foreach (KeyValuePair<int, int[]> d in dict)
+                                {
+                                    //pobieramy z dictionary wspolrzedne obu wierzchołków
+
+                                    if (d.Key == i)
+                                    {
+                                        Pwspx = d.Value[0];
+                                        Pwspy = d.Value[1];
+                                    }
+
+                                    else if (d.Key == j)
+                                    {
+                                        Dwspx = d.Value[0];
+                                        Dwspy = d.Value[1];
+                                    }
+                                }
+                            
+                            graphicsObj.DrawLine(czarny, Pwspx + 6, Pwspy + 6, Dwspx + 6, Dwspy + 6); //rysujemy krawędź pomiędzy wierzchołkami
+                            break;
+
+                        case 1:
+                            if (tab[i, j] == 0 && i != j) //jesli natrafiamy na 0 w macierzy to:                    
+                                foreach (KeyValuePair<int, int[]> d in dict)
+                                {
+                                    //pobieramy z dictionary wspolrzedne obu wierzchołków
+
+                                    if (d.Key == i)
+                                    {
+                                        Pwspx = d.Value[0];
+                                        Pwspy = d.Value[1];
+                                    }
+
+                                    else if (d.Key == j)
+                                    {
+                                        Dwspx = d.Value[0];
+                                        Dwspy = d.Value[1];
+                                    }
+                                }
+                            
+                            graphicsObj.DrawLine(zielony, Pwspx + 6, Pwspy + 6, Dwspx + 6, Dwspy + 6); //rysujemy krawędź pomiędzy wierzchołkami
+                            break;
+                        default:
+                            if (i != j)
+                                foreach (KeyValuePair<int, int[]> d in dict)
+                                {
+                                    //pobieramy z dictionary wspolrzedne obu wierzchołków
+                                    if (d.Key == i)
+                                    {
+                                        Pwspx = d.Value[0];
+                                        Pwspy = d.Value[1];
+                                    }
+                                
+                                    else if (d.Key == j)
+                                    {
+                                        Dwspx = d.Value[0];
+                                        Dwspy = d.Value[1];
+                                    }
+                                }                                
+                            if (tab[i,j] == 1)
+                                graphicsObj.DrawLine(czarny, Pwspx + 6, Pwspy + 6, Dwspx + 6, Dwspy + 6); //rysujemy krawędź pomiędzy wierzchołkami
+                            else
+                                graphicsObj.DrawLine(zielony, Pwspx + 6, Pwspy + 6, Dwspx + 6, Dwspy + 6); //rysujemy krawędź pomiędzy wierzchołkami
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void grafDopełniajacyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rysowanie(700, 150, 1);
+        }
+
+        private void razemToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rysowanie(150, 380, 2);
+        }
+
+
+
+    }
+}
